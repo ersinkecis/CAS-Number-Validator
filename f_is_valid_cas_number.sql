53c6ab734c7f54b6ae680f3c3945dda4306f8874
Ersin Kecis <ersinkecis@users.noreply.github.com>
2019-10-15 14:53:11 +0300

    Converted to Microsoft SQL Server version
    Converted to Microsoft SQL Server version

--- f_is_valid_cas_number.sql
+++ f_is_valid_cas_number.sql
@@ -1,5 +1,12 @@
-CREATE OR REPLACE FUNCTION f_is_valid_cas_number(p_cas_number IN VARCHAR2)
-  return NUMBER is
+USE [Spectrums]
+GO
+/****** Object:  StoredProcedure [dbo].[p_is_valid_cas_number]    Script Date: 15.10.2019 01:06:49 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[p_is_valid_cas_number](@p_cas_number AS VARCHAR(50)) --'1234-56-7'
+AS
 
 /*
  * Function to validate a CAS Registry Number / CAS Number / CASRN
@@ -11,57 +18,102 @@ CREATE OR REPLACE FUNCTION f_is_valid_cas_number(p_cas_number IN VARCHAR2)
  * isotopes, alloys and nonstructurable materials (UVCBs, of unknown, variable
  * composition, or biological origin).
  *
- * @author		Abhinav Sood
- * @author_url	http://www.abhinavsood.com/
- * @link		http://www.abhinavsood.com/cas-number-validation/
- * @version		1.0
+ * @author1        Abhinav Sood (Oracle - PL/SQL version)
+ * @author1_url    http://www.abhinavsood.com/
+ * @ora_link       http://www.abhinavsood.com/cas-number-validation/
+
+
+
+
+
+ * @author2        Ersin Kecis (MSSQL Server - T/SQL version)
+ * @author2_url    https://github.com/ersinkecis/
+ * @sql_link       https://github.com/ersinkecis/CAS-Number-Validator
+ *
+ * @checksum       https://www.cas.org/support/documentation/chemical-substances/checkdig
+ *                 http://www.caslab.com/Validate-CAS-Number/index.php
+ *                 https://www.wikizeroo.org/wiki/en/CAS_Registry_Number
+ *                 CAS number of water is 7732-18-5: the checksum 5 is calculated as 
+ *                 (8×1 + 1×2 + 2×3 + 3×4 + 7×5 + 7×6) = 105 mod 10 = 5.
+ *
+ *                 CAS No: 1002-84-2  (Pentadecanoic Acid)
+ *                 digits: 6543-21-X  (Check digit not used)
+ *                 1 * 6 =  6
+ *                 0 * 5 =  0
+ *                 0 * 4 =  0
+ *                 2 * 3 =  6
+ *                 8 * 2 = 16
+ *                 4 * 1 =  4
+ *                 Checksum = (6 + 0 + 0 + 6 + 16 + 4) MOD 10 = (32 MOD 10) = 2
+ *
+ *                 A CAS Registry Number® includes up to 10 digits which are separated into 3 groups by hyphens. 
+ *                 The first part of the number, starting from the left, has 2 to 7 digits; the second part has 2 digits. 
+ *                 The final part consists of a single check digit. The format is 'xxxxxxx-yy-z' (look 1th checksum link)
+ * @version        1.0 (t-sql)
 **/
 
-  -- Last digit of the CAS Number is the check digit
-  check_digit NUMBER(1);
+BEGIN
+  -- Last digit of the CAS Number is the check digit (checksum)
+  DECLARE @check_digit NUMERIC;
+  DECLARE @checksum_source_length NUMERIC;
 
   -- Remove HYPHENs from CAS Number and compute checksum using all but the last digit
-  checksum_source        NUMBER;
-  checksum_source_length NUMBER;
+  DECLARE @reversed_cas_number VARCHAR(50);
+  SET @reversed_cas_number = (SELECT REVERSE(@p_cas_number)); --'7-65-4321' -> '7654321'
+  SET @reversed_cas_number = REPLACE(@reversed_cas_number,'-',''); --'7-65-4321' -> '7654321'
+  SET @reversed_cas_number = (SELECT SUBSTRING(@reversed_cas_number, 2, LEN(@reversed_cas_number) - 1)); --'654321'
 
   -- Computed checksum.
   -- Check Digit should be equal to this checksum for the CAS number to be valid
-  checksum NUMBER := 0;
+  SET @check_digit = CONVERT(NUMERIC, SUBSTRING(@p_cas_number, LEN(@p_cas_number), 1)); --'7'
 
   -- Set to 1 if format of input CAS number is valid
-  is_format_valid NUMBER(1) := 0;
+  DECLARE @is_format_valid NUMERIC(1) = 0;
+  DECLARE @nval1 NUMERIC;
+  DECLARE @nval2 NUMERIC;
+  DECLARE @nval3 NUMERIC;
+  BEGIN TRY
+      --DECLARE @val1 VARCHAR(50) = (SELECT value FROM (SELECT ROW_NUMBER() OVER(ORDER BY (SELECT NULL)) AS rownum, value FROM STRING_SPLIT(@p_cas_number, '-')) t WHERE t.rownum=1); --'1234'
+      --DECLARE @val2 VARCHAR(50) = (SELECT value FROM (SELECT ROW_NUMBER() OVER(ORDER BY (SELECT NULL)) AS rownum, value FROM STRING_SPLIT(@p_cas_number, '-')) t WHERE t.rownum=2); --'56'
+      --DECLARE @val3 VARCHAR(50) = (SELECT value FROM (SELECT ROW_NUMBER() OVER(ORDER BY (SELECT NULL)) AS rownum, value FROM STRING_SPLIT(@p_cas_number, '-')) t WHERE t.rownum=3); --'7'
+      SET @nval1 = (SELECT CONVERT(NUMERIC, value) AS nval1 FROM (SELECT ROW_NUMBER() OVER(ORDER BY (SELECT NULL)) AS rownum, value FROM STRING_SPLIT(@p_cas_number, '-')) t WHERE t.rownum=1); --1234
+      SET @nval2 = (SELECT CONVERT(NUMERIC, value) AS nval2 FROM (SELECT ROW_NUMBER() OVER(ORDER BY (SELECT NULL)) AS rownum, value FROM STRING_SPLIT(@p_cas_number, '-')) t WHERE t.rownum=2); --56
+      SET @nval3 = (SELECT CONVERT(NUMERIC, value) AS nval3 FROM (SELECT ROW_NUMBER() OVER(ORDER BY (SELECT NULL)) AS rownum, value FROM STRING_SPLIT(@p_cas_number, '-')) t WHERE t.rownum=3); --7
+      SET @is_format_valid = 1;
+  END TRY
+  BEGIN CATCH
+      SET @is_format_valid = 0;
+  END CATCH
 
   -- Set to 1 if format as well as checksum of input CAS Number is valid
-  is_valid_cas NUMBER(1) := 0;
-BEGIN
-  SELECT REGEXP_INSTR(p_cas_number, '^\d{1,7}-\d{2}-\d$'),
-         TRANSLATE(p_cas_number, 'a-', 'a'),
-         LENGTH(TRANSLATE(p_cas_number, 'a-', 'a')) - 1,
-         SUBSTR(p_cas_number, -1, 1)
-    INTO is_format_valid,
-         checksum_source,
-         checksum_source_length,
-         check_digit
-    FROM dual;
+  DECLARE @is_valid_cas NUMERIC = 0;
 
   -- If format is valid then compute checksum and validate the check digit against it
-  IF is_format_valid = 1 THEN
-    FOR i IN 1 .. checksum_source_length LOOP
-      checksum := checksum + (SUBSTR(checksum_source, i, 1) *
-                  (checksum_source_length - i + 1));
-    END LOOP;
+  DECLARE @temp NUMERIC = 0;
+  DECLARE @checksum NUMERIC = 0;
+  DECLARE @checksum_source VARCHAR(50);
+  SET @checksum_source = @reversed_cas_number;
+  SET @checksum_source_length = (SELECT LEN(@checksum_source));
+  IF (@is_format_valid = 1)
+  BEGIN
+    SET @checksum = 0;
+    DECLARE @i int = 0
+    WHILE @i < @checksum_source_length
+    BEGIN
+        SET @i = @i + 1
+        SET @temp = CONVERT(NUMERIC, SUBSTRING(@checksum_source, @i, 1))  * @i;
+        SET @checksum = @checksum + @temp;
+    END
+  END
+ 
+  SET @temp = @checksum % 10;
+  SET @checksum = @temp;
 
-    checksum := MOD(checksum, 10);
+  -- CAS Number is valid only if checksum matches with the check digit
+  IF (@checksum = @check_digit)
+  BEGIN
+      SET @is_valid_cas = 1;
+  END
 
-    -- CAS Number is valid only if checksum matches with the check digit
-    IF checksum = check_digit THEN
-      is_valid_cas := 1;
-    END IF;
-  END IF;
-
-  RETURN(is_valid_cas);
-EXCEPTION
-  WHEN VALUE_ERROR THEN
-    RETURN(is_valid_cas);
-END f_is_valid_cas_number;
- 	
+  SELECT @is_valid_cas;
+END
